64498c97052649ebadd3f94514054f3109247d90
Matt Insko <minsko@preemptive.com>
2019-04-17 10:31:52 -0400

    Update to AndroidX

--- app/src/paid/java/com/preemptive/dasho/example/gameoflife/AbstractGameOfLifeActivity.java
+++ app/src/paid/java/com/preemptive/dasho/example/gameoflife/AbstractGameOfLifeActivity.java
@@ -1,7 +1,7 @@
 package com.preemptive.dasho.example.gameoflife;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Toast;
 
 import io.kimo.gameoflifeview.view.GameOfLifeView;
